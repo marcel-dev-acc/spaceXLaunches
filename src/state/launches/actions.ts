863f8031e383befb39a027808063233db217e7bb
Marcel Mulders <marcelm4242@gmail.com>
2022-12-12 16:02:44 +0000

    feat: added actions with type definitions for global state

---
+++ src/state/launches/actions.ts
@@ -0,0 +1,25 @@
+import type { Rocket } from "../../types/type.launches";
+import { LAUNCHES_ACTION_TYPES } from "../../constants/state";
+import type { AddLaunchAction, RemoveLaunchAction } from "../types";
+
+export const addLaunch = (
+  flight_number: number,
+  mission_name: string,
+  launch_date_unix: number,
+  rocket: Rocket,
+): AddLaunchAction => ({
+  type: LAUNCHES_ACTION_TYPES.LAUNCH_ADDED,
+  launchData: {
+    flight_number,
+    mission_name,
+    launch_date_unix,
+    rocket,
+  }
+});
+
+export const removeLaunch = (
+  flightNumber: number,
+): RemoveLaunchAction => ({
+  type: LAUNCHES_ACTION_TYPES.LAUNCH_REMOVED,
+  flightNumber: flightNumber,
+});
---
+++ src/state/types.ts
@@ -0,0 +1,22 @@
+// Types specific to global state
+import { Launch } from "../types/type.launches";
+
+export type LaunchesState = Launch[];
+
+export type AddLaunchAction = {
+    type: string;
+    launchData: Launch;
+};
+
+export type UpdateLaunchAction = {
+    type: string;
+    flightNumber: number;
+    launchData: Launch;
+};
+
+export type RemoveLaunchAction = {
+    type: string;
+    flightNumber: number;
+};
+
+export type LaunchListAction = AddLaunchAction | UpdateLaunchAction | RemoveLaunchAction;
