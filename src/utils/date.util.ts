e4cf4c04e825bd1da3168d3cfa053fe47ba22b46
Marcel Mulders <marcelm4242@gmail.com>
2022-12-12 15:31:02 +0000

    feat: added date functionality for launches

---
+++ src/utils/date.util.ts
@@ -0,0 +1,11 @@
+import * as dayjs from 'dayjs';
+import AdvancedFormat from 'dayjs/plugin/advancedFormat'
+
+dayjs.extend(AdvancedFormat);
+
+const wrapTimestamp = (timestamp: number) => dayjs.unix(timestamp);
+
+export const formatTimestamp = (timestamp: number, fmt: string) => {
+  const d = wrapTimestamp(timestamp);
+  return d.format(fmt);
+};
